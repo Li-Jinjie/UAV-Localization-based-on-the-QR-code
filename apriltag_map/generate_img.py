77a36402adf233e7b70568cf00950b958f6ce17c
Li-Jinjie <lijinjie362@outlook.com>
2021-05-07 16:47:23 +0800

    to generate apriltag map based on the height and width

--- apriltag_map/generate_img.py
+++ apriltag_map/generate_img.py
@@ -2,12 +2,12 @@
 # coding=utf-8
 '''
 @Author       : LI Jinjie
-@Date         : 1970-01-01 08:00:00
+@Date         : 2020-03-10 18:23:19
 @LastEditors  : LI Jinjie
 @LastEditTime : 2020-03-10 18:23:19
 @Units        : Meters
 @Description  : This python script can generate an image with apriltags arranged as you wish
-@Dependencies : None
+@Dependencies : 注意opencv，左上角是图像原点
 @NOTICE       : None
 '''
 
@@ -15,29 +15,37 @@ import os
 import numpy as np
 import cv2
 
+HEIGHT = 2160  # pixels of the whole map's height
+WIDTH = 1920  # pixels of the whole map's width
 
-BORDER = 0.3  # The length of one BLACK side. Unit: m
-PIXELS_P_METER = 240  # pixels per meter. 确认BORDER×PIXELS_P_METER是PIXELS_P_SIDE的整数倍
+METER_FLAG = False  # use meter or pixels
+BORDER = 0.2  # The length of one BLACK side. Unit: m
+PIXELS_P_METER = 1920  # pixels per meter. 确认BORDER×PIXELS_P_METER是PIXELS_P_SIDE的整数倍
+
 PIXELS_P_SIDE = 10  # 原始二维码一边有几个像素
 
-DISTANCE = 0.9  # The horizontal distance between 2 apriltags
+DISTANCE = 0.3  # The horizontal distance between 2 apriltags
 # DISTANCE_H = 1.0  # The verstical distance between 2 apriltags
-NUM = 9  # The number of tags in one side. 必须是奇数！
+NUM = 3  # The number of tags in one side. Must be an odd number!
 # NUM_H = 5  # The number of tags in height
-WIDTH_OUT = 0.5  # 最外边留白的宽度
+WIDTH_OUT = 0.5  # 最外边留白的宽度, meter
 
-
-dirPath = "Pictures/"
+dirPath = "./Pictures/"
 
 if __name__ == "__main__":
     if NUM % 2 == 0:
         raise ValueError('一边的二维码数量NUM必须为奇数！请检查')
 
-    borderPix = int(BORDER * PIXELS_P_METER)
-    widthAllM = (DISTANCE * (NUM-1) + BORDER * 1 + WIDTH_OUT * 2)  # unit: m
-    AllNum = NUM * NUM
-    widthAllP = int(widthAllM * PIXELS_P_METER)
+    if METER_FLAG is True:
+        widthAllM = (DISTANCE * (NUM - 1) + BORDER * 1 + WIDTH_OUT * 2)  # unit: m
+        widthAllP = int(widthAllM * PIXELS_P_METER)
+        heightAllP = int(widthAllM * PIXELS_P_METER)
+    else:
+        widthAllP = WIDTH
+        heightAllP = HEIGHT
 
+    borderPix = int(BORDER * PIXELS_P_METER)
+    AllNum = NUM * NUM
     imgNames = os.listdir(dirPath)
     imgNames.sort()
     if imgNames[0] != 'tag36_11_00000.png':
@@ -45,57 +53,63 @@ if __name__ == "__main__":
             'There is other files in the Picture folder. Please check!')
 
     # ================制作图片=============
-    imgWhite = np.ones((widthAllP, widthAllP), dtype=np.uint8)
-    imgWhite = imgWhite * 255
+    imgNoTag = np.ones((heightAllP, widthAllP), dtype=np.uint8)
+    imgNoTag = imgNoTag * 127
 
-    origin = np.array((widthAllP - 1 - WIDTH_OUT*PIXELS_P_METER,
-                       WIDTH_OUT*PIXELS_P_METER))  # 是第一个二维码左下角的位置，左下为原点
+    center = np.array([heightAllP / 2, widthAllP / 2])
+
+    origin = np.array([center[0] + (NUM - 1) / 2 * DISTANCE * PIXELS_P_METER + BORDER / 2 * PIXELS_P_METER - 1,
+                       center[1] - (NUM - 1) / 2 * DISTANCE * PIXELS_P_METER - BORDER / 2 * PIXELS_P_METER])
+    # origin = np.array((widthAllP - 1 - WIDTH_OUT*PIXELS_P_METER,
+    #                    WIDTH_OUT*PIXELS_P_METER))  # 是第一个二维码左下角的位置，左下为原点
 
     for i, name in enumerate(imgNames):
         if i == AllNum:
             break
         img = cv2.imread(dirPath + name, flags=cv2.IMREAD_GRAYSCALE)
-        img = img[1:9, 1:9]   # remove the white border
+        # img = img[1:9, 1:9]  # remove the white border
         imgBig = cv2.resize(img, (borderPix, borderPix),
                             interpolation=cv2.INTER_NEAREST)
 
         dw = int(i % NUM) * DISTANCE * PIXELS_P_METER
         dh = int(i / NUM) * DISTANCE * PIXELS_P_METER
         coor_lb = np.array((-dh, dw)) + origin
-        coor_rt = coor_lb + np.array((-borderPix+1, borderPix-1))
+        coor_rt = coor_lb + np.array((-borderPix + 1, borderPix - 1))
         coor_lt = np.array((coor_rt[0], coor_lb[1]), dtype=int)
 
-        imgWhite[coor_lt[0]:coor_lt[0]+borderPix,
-                 coor_lt[1]:coor_lt[1]+borderPix] = imgBig
+        imgNoTag[coor_lt[0]:coor_lt[0] + borderPix,
+        coor_lt[1]:coor_lt[1] + borderPix] = imgBig
 
     # # 检查中心二维码的位置是否与图片的中心重叠
     # imgWhite[int(widthAllP/2-1):int(widthAllP/2+1), :] = 0
     # imgWhite[:, int(widthAllP/2-1):int(widthAllP/2+1)] = 0
 
-    imgWhite[widthAllP-10:widthAllP-5, 5:10] = 0
-    cv2.imshow('imgFinal', imgWhite)
+    # imgNoTag[widthAllP - 10:widthAllP - 5, 5:10] = 255  # a small tag
+
+    imgTag = imgNoTag
+    cv2.imshow('imgFinal', imgTag)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     cv2.imwrite('apriltagMap_%dx%d_%.4fm.png' %
-                (NUM, NUM, widthAllM), imgWhite)
+                (NUM, NUM, WIDTH), imgTag)
 
     # =================制作文本=================
-    txtName = 'apriltagMapInfo_%dx%d_%.4fm.txt' % (NUM, NUM, widthAllM)
+    txtName = 'apriltagMapInfo_%dx%d_%.4fm.txt' % (NUM, NUM, WIDTH)
     fp = open(txtName, 'w')
     fp.write("name: 'my_bundle',\nlayout:\n  [\n")
 
     for i, name in enumerate(imgNames):
-        originM = (-(NUM-1)/2*DISTANCE, -(NUM-1) /
-                   2*DISTANCE)  # 坐标变换，改成中间的二维码为原点
+        originM = (-(NUM - 1) / 2 * DISTANCE, -(NUM - 1) /
+                   2 * DISTANCE)  # 坐标变换，改成中间的二维码为原点
         dwM = int(i % NUM) * DISTANCE
         dhM = int(i / NUM) * DISTANCE
-        if i == AllNum-1:
+        if i == AllNum - 1:
             text = "    {id: %d, size: %.2f, x: %.4f, y: %.4f, z: 0.0, qw: 1.0, qx: 0.0, qy: 0.0, qz: 0.0}\n  ]" % (
-                int(i), BORDER, originM[0]+dwM, originM[1]+dhM)
+                int(i), BORDER, originM[0] + dwM, originM[1] + dhM)
             fp.writelines(text)
             break
         text = "    {id: %d, size: %.2f, x: %.4f, y: %.4f, z: 0.0, qw: 1.0, qx: 0.0, qy: 0.0, qz: 0.0},\n" % (
-            int(i), BORDER, originM[0]+dwM, originM[1]+dhM)
+            int(i), BORDER, originM[0] + dwM, originM[1] + dhM)
         fp.writelines(text)
 
     fp.close()  # 不要忘记
