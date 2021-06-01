ca30fa0d77323529c4326f333fbffc4b6f4deb7c
Li-Jinjie <lijinjie362@outlook.com>
2021-06-01 22:43:21 +0800

    load apriltag map information

---
+++ projected_apriltag/map_info_loading.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+Author: LI Jinjie
+File: map_info_loading.py
+Date: 2021/6/1 22:35
+LastEditors: LI Jinjie
+LastEditTime: 2021/6/1 22:35
+Description: load map information
+'''
+import yaml
+
+path = "D:\\ForGithub\\UAV-Localization-based-on-the-QR-code\\apriltag_map\\maps_info.yaml"
+with open(path, 'r') as file:
+    map_info = yaml.load(file, Loader=yaml.FullLoader)
+    layout = map_info['layout']
+    print(layout[0])
