b3883801f40592280fabff36ca9a2bd48a404590
Li-Jinjie <lijinjie362@outlook.com>
2021-06-02 10:48:24 +0800

    finish camera_calibration.py

---
+++ camera_calibration.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+Author: LI Jinjie
+File: camera_calibration.py
+Date: 2021/6/2 10:05
+LastEditors: LI Jinjie
+LastEditTime: 2021/6/2 10:05
+Description: to calibrate the camera
+'''
+# %%
+import numpy as np
+import cv2 as cv
+import glob
+
+path = "experiments_data_real\\20210530_full_data\\camera_calibration_data\\"
+
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6 * 9, 3), np.float32)
+objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+images = glob.glob(path + '*.jpg')
+for i, fname in enumerate(images):
+    img = cv.imread(fname)
+    print("image ", i)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9, 6), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (9, 6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
+# %%
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# %%
+np.savez(path + 'GZ120', mtx, dist)
+print("Finish calibration!")
